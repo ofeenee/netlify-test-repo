8e6ecdd905f6bc03f883b7f2cc7e29da7520d7dc
ofeenee <ofeenee@gmail.com>
2023-04-23 19:09:01 +0300

    remove unnecessary type annotations from hooks.server.ts (sveltekit inferred automatically)

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,28 +1,26 @@
-import type { Handle } from '@sveltejs/kit';
-import { PUBLIC_ANON, PUBLIC_URL } from '$env/static/public';
-import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
+import { PUBLIC_ANON, PUBLIC_URL } from "$env/static/public";
+import { createSupabaseServerClient } from "@supabase/auth-helpers-sveltekit";
 
-export const handle = (async ({ resolve, event }) => {
-    event.locals.supabase = createSupabaseServerClient({
-        supabaseUrl: PUBLIC_URL,
-        supabaseKey: PUBLIC_ANON,
-        event
-    });
+export async function handle({ resolve, event }) {
+  event.locals.supabase = createSupabaseServerClient({
+    supabaseUrl: PUBLIC_URL,
+    supabaseKey: PUBLIC_ANON,
+    event,
+  });
 
-    event.locals.getSession = async () => {
-        const {
-            data: { session }
-        } = await event.locals.supabase.auth.getSession();
-        return session;
-    };
+  event.locals.getSession = async () => {
+    const {
+      data: { session },
+    } = await event.locals.supabase.auth.getSession();
+    return session;
+  };
 
-    const types = ["css", "font", "js"];
-    const response = await resolve(event, {
-        preload: ({ type }) => types.includes(type),
-        filterSerializedResponseHeaders(name) {
-            return name === 'content-range';
-          }
-    });
-    return response;
-
-}) satisfies Handle
+  const types = ["css", "font", "js"];
+  const response = await resolve(event, {
+    preload: ({ type }) => types.includes(type),
+    filterSerializedResponseHeaders(name) {
+      return name === "content-range";
+    },
+  });
+  return response;
+}
