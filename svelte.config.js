9a7cb69a28f52f131ecb44f5e8f8eff1867f85c9
ofeenee <ofeenee@gmail.com>
2023-04-23 19:07:47 +0300

    add kit.prerender.force: true to svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -20,12 +20,17 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
+
 			// runtime: 'nodejs18.x',
 			edge: true
 		}),
 		csrf: {
 			checkOrigin: DEV
+		},
+		prerender: {
+			force: true,
 		}
+		// outDir: 'build',
 	}
 };
 
